1e50e60ce62066a4054a25edb1e8de34468b73b6
David Mejia <sc2terranman@gmail.com>
2016-03-14 00:18:29 -0700

    updated ball

--- BallGame/Assets/Scripts/CameraBeh.cs
+++ BallGame/Assets/Scripts/CameraBeh.cs
@@ -4,36 +4,37 @@ using System.Collections;
 public class CameraBeh : MonoBehaviour
 {
     public GameObject m_ObjToFollow;
+    public float m_RotationSpeed = 15;
     private Transform m_ObjTrans;
-    private Vector3 m_Offset;
+    //private Vector3 m_Offset;
+    private Vector3 objLastFramePos;
     public void Init(GameObject _obj)
     {
         //what object the camera will follow
         m_ObjToFollow = _obj;
         m_ObjTrans = m_ObjToFollow.transform;
-        m_Offset = transform.position - m_ObjTrans.position;
-
+      //  m_Offset = transform.position - m_ObjTrans.position;
+        //transform.position += m_ObjTrans.position + m_Offset;
+        objLastFramePos = m_ObjTrans.position;
+        
     }
-
-    // Update is called once per frame
-    void LateUpdate()
+    public void MoveCamera(float _xAmount, float _yAmount)
     {
-#if true
-       
-        transform.position = m_ObjTrans.position + m_Offset;
+        //how much to move the camera from last from to this frame
+        Vector3 distanceTraveled = m_ObjTrans.position - objLastFramePos; 
+        //look at the ball
         transform.LookAt(m_ObjTrans);
-#else
-        float damping = 1.0f;
-        float currentAngle = transform.eulerAngles.y;
-        float desiredAngle = m_ObjTrans.eulerAngles.y;
-        float angle = Mathf.LerpAngle(currentAngle, desiredAngle, damping);
-
-        Quaternion rotation = Quaternion.Euler(0, angle, 0);
-        transform.position = m_ObjTrans.position - (rotation * m_Offset);
-
-#endif
+        //how much to rotate
+        Vector3 rotationY = Vector3.left * _xAmount * m_RotationSpeed * Time.deltaTime;
+        Vector3 rotationX = Vector3.up   * _yAmount * m_RotationSpeed * Time.deltaTime;
+        //move the camera based off of rotation
+        transform.Translate(rotationX);
+        transform.Translate(rotationY);
+        //move the camera based off of the distance between the ball
+        transform.position += distanceTraveled;
+        //update the last frame position
+        objLastFramePos = m_ObjTrans.position;
     }
-    
     
 
 }
